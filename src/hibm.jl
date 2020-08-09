6384e6b985c7e3e64caa8e19aea994faef93a10a
h4pZ <h4pz@protonmail.com>
2020-08-08 19:43:09 -0500

    Update hibm.jl theme

--- src/hibm.jl
+++ src/hibm.jl
@@ -2,25 +2,33 @@
 colors = [
     colorant"#f60157", 
     colorant"#0157f6", 
-    colorant"#fcfc50",
-    colorant"#99ffcc",
-    colorant"#607889",
-    colorant"#70396a"
+    colorant"#5202a3",
+    colorant"#f2741d",
+    colorant"#d3576d",
+    colorant"#f3ed22",
+    colorant"#99ffcc"
 ]
-]
+
 background = colorant"#171616"
 
 
 _themes[:hibm] = PlotTheme(
     bg = background,
     bginside = background,
+    titlefontcolor=colorant"#ffffff",
     fg = colorant"#96999b",
-    palette = expand_palette(background, colors; lchoices = [57], cchoices = [100]),
-    colorgradient = :plasma,
     fgtext = colorant"#96999b",
     fgguide = colorant"#96999b",
     fglegend = colorant"#96999b",
     legendfontcolor = colorant"#96999b",
     legendtitlefontcolor = colorant"#96999b",
+    palette = expand_palette(background, colors; lchoices = [57], cchoices = [100]),
+    colorgradient = :plasma,
+    gridstyle = :dot,
+    markerstrokecolor = colorant"#ffffff",
+    markerstrokealpha = 0.0,
+    titlefontsize = 10,
+    legendfontsize = 8,
+    tickfontsize = 8,
     fontfamily="monospace"
 )
